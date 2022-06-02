78400e93e957617d499ddc63cdfdd1528d7ffa26
Fabricio Breve <fbreve@gmail.com>
2022-06-01 22:36:06 -0300

    Update example.m

--- example.m
+++ example.m
@@ -17,7 +17,7 @@ label = wine(:,1);
 % An unlabeled item is represented by 0.
 slabel = slabelgenwl(label,0.1,0.3);
 disp('Parameters k: 10, distance: Normalized Euclidean, others: Default.');
-disp('Running the algorithm on the Wine Dat Set with 10% labeled examples, from which 30% have a wrong label (label noise)...');
+disp('Running the algorithm on the Wine Dat Set with 10% labeled examples, from which 30% have wrong labels (label noise)...');
 tStart = tic;
 owner = lnpcc(X, slabel, 10, 'seuclidean');
 tElapsed = toc(tStart);
